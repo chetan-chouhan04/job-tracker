28ca7c1cbbd257cb7dd74262a6ad47cf8aae0068
chetan chouhan <chetanchouhan910@gmail.com>
2026-06-09 15:27:30 +0530

    Update API URL to production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://job-tracker-backend-9mbh.onrender.com/api',
 });
 
 instance.interceptors.request.use((config) => {
